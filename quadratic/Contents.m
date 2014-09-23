79b2d8afca7ae412acf17817c2f7fd9629028b7b
Jordy van Velthoven <jordyvanvelthoven@imac-van-jordy.home>
2014-09-23 20:58:39 +0200

    Updated contents file for quadratic section.

--- quadratic/Contents.m
+++ quadratic/Contents.m
@@ -1,5 +1,18 @@
-% LTFAT - Quadratic TF distributions
+% LTFAT - Quadratic time-frequency distributions
 %
 %   Jordy van Velthoven, 2014.
 %
+%	General quadratic distribution
+%	 DQTFD			-  discrete quadratic time-frequency distribution
+%
+%	Quadratic distributions
+%    	 DAF            	-  discrete ambiguity function
+%	 DBJD			-  discrete Born-Jordan distribution
+%	 DCWD			-  discrete Choi-Williams distribution
+%	 DMHD			-  discrete Margenau-Hill distribution
+%	 DRD			-  discrete Rihaczek distribution
+%	 DWVD			-  discrete Wigner-Ville distribution
+%
+%	Plotting routine
+%	 PLOTTFD		- Plot time-frequency distribution
 %
