4444f2c24a4a6ffb98fe29e496071a36d4e42401
Jordy van Velthoven <jordyvanvelthoven@imac-van-jordy.home>
2014-10-16 17:20:17 +0200

    Changed name of Rihaczek distribution from 'drd' to 'drihaczekdist'.

--- quadratic/drihaczekdist.m
+++ quadratic/drihaczekdist.m
@@ -1,6 +1,6 @@
-function r = drd(f);
-%DRD discrete Rihaczek distribution
-%   Usage W = drd(f);
+function r = drihaczekdist(f);
+%DRIHACZEKDIST discrete Rihaczek distribution
+%   Usage W = drihaczekdist(f);
 %
 %   Input parameters:
 %         f      : Input signal
@@ -8,7 +8,7 @@ function r = drd(f);
 %   Output parameters:
 %         d      : discrete Rihaczek distribution
 %
-% `drd(f)` computes a discrete discrete Rihaczek distribution.
+% `drihaczekdist(f)` computes a discrete discrete Rihaczek distribution.
 % The discrete Rihaczek distribution is computed by
 %
 % .. math:: r\left( k+1,\; l+1 \right)\; =\; f\left( l+1 \right)\; \overline{c\left( k+1 \right)}e^{-2\pi ikl/L}
@@ -16,10 +16,10 @@ function r = drd(f);
 % where $k, l=0,\ldots,L-1$ and $c$ is the Fourier transform of $f$.
 
 % AUTHOR: Jordy van Velthoven
-% TESTING: TEST_DRD
-% REFERENCE: REF_DRD
+% TESTING: TEST_DRIHACZEKDIST
+% REFERENCE: REF_DRIHACZEKDIST
 
-complainif_notenoughargs(nargin, 1, 'DRD');
+complainif_notenoughargs(nargin, 1, 'DRIHACZEKDIST');
 
 [f,~,Ls,W,~,permutedsize,order]=assert_sigreshape_pre(f,[],[],upper(mfilename));
 
--- testing/test_drihaczekdist.m
+++ testing/test_drihaczekdist.m
@@ -1,9 +1,9 @@
-function test_failed = test_drd
+function test_failed = test_drihaczekdist
 Lr = [1, 19, 20];
 
 test_failed = 0;
 
-disp(' ===============  TEST_DRD ==============');
+disp(' ===============  TEST_DRIHACZEKDIST ==============');
 
 for ii = 1: length(Lr)
   L = Lr(ii);
@@ -15,13 +15,13 @@ for ii = 1: length(Lr)
     f = tester_crand(L,1);
     end
   
-    r1 = ref_drd(f);
-    r2 = drd(f);
+    r1 = ref_drihaczekdist(f);
+    r2 = drihaczekdist(f);
   
     res = norm(r1-r2);
   
     [test_failed, fail] = ltfatdiditfail(res, test_failed);
-    s = sprintf('DRD %3s L:%3i %0.5g %s', type{1}, L, res, fail);
+    s = sprintf('DRIHACZEKDIST %3s L:%3i %0.5g %s', type{1}, L, res, fail);
     disp(s);
     end
 end
