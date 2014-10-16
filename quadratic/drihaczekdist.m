6078e7ba936144a1f72ec22a0cc6a89fe025bb82
Jordy van Velthoven <jordyvanvelthoven@imac-van-jordy.home>
2014-10-16 17:42:15 +0200

    Small documentation fix.

--- quadratic/drihaczekdist.m
+++ quadratic/drihaczekdist.m
@@ -1,23 +1,18 @@
 function r = drihaczekdist(f);
 %DRIHACZEKDIST discrete Rihaczek distribution
-%   Usage W = drihaczekdist(f);
+%   Usage r = drihaczekdist(f);
 %
-%   Input parameters:
-%         f      : Input signal
 %
-%   Output parameters:
-%         d      : discrete Rihaczek distribution
+%   `drihaczekdist(f)` computes a discrete discrete Rihaczek distribution.
+%   The discrete Rihaczek distribution is computed by
 %
-% `drihaczekdist(f)` computes a discrete discrete Rihaczek distribution.
-% The discrete Rihaczek distribution is computed by
+%   .. math:: r\left( k+1,\; l+1 \right)\; =\; f\left( l+1 \right)\; \overline{c\left( k+1 \right)}e^{-2\pi ikl/L}
 %
-% .. math:: r\left( k+1,\; l+1 \right)\; =\; f\left( l+1 \right)\; \overline{c\left( k+1 \right)}e^{-2\pi ikl/L}
-%
-% where $k, l=0,\ldots,L-1$ and $c$ is the Fourier transform of $f$.
+%   where $k, l=0,\ldots,L-1$ and $c$ is the Fourier transform of $f$.
 
-% AUTHOR: Jordy van Velthoven
-% TESTING: TEST_DRIHACZEKDIST
-% REFERENCE: REF_DRIHACZEKDIST
+%   AUTHOR: Jordy van Velthoven
+%   TESTING: TEST_DRIHACZEKDIST
+%   REFERENCE: REF_DRIHACZEKDIST
 
 complainif_notenoughargs(nargin, 1, 'DRIHACZEKDIST');
 
